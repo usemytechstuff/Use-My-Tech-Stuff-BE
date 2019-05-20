5f13ecca3be986e9b951360b9489d145bfbb7257
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-20 14:03:27 -0700

    test

--- helpers/users/usersModel.js
+++ helpers/users/usersModel.js
@@ -19,11 +19,14 @@ function findBy(filter) {
 }
 
 async function add(user) {
+  console.log('firing')
   const [id] = await db('users').insert(user, "id");
+  console.log(id)
   return findById(id);
 }
 
 function findById(id) {
+  console.log('next')
   return db('users')
     .where({ id })
     .first();
