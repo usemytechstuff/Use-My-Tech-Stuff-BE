6786d5f21ce36b7ca77232f9a8a89b8be35e7c7e
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-22 14:11:39 -0700

    addItem testing passed

--- helpers/items/itemsModel.spec.js
+++ helpers/items/itemsModel.spec.js
@@ -1,5 +1,6 @@
 const db = require('../../data/dbConfig');
 const items = require('../items/itemsModel');
+const users = require('../users/usersModel');
 
 describe('items model', () => {
     beforeEach(async () => {
@@ -7,6 +8,12 @@ describe('items model', () => {
     });
     afterEach(async () => {
         await db("items").truncate();
+    });
+    beforeEach(async () => {
+        await db("users").truncate();
+    });
+    afterEach(async () => {
+        await db("users").truncate();
     });
 
     const mockItem = {
@@ -19,10 +26,28 @@ describe('items model', () => {
         brand: "Canon", 
         model: "EOS 200"
     }
+
+    const mockUser = {
+        username: "JohnSmith123", 
+        password: "password", 
+        email: "JohnSmith123@gmail.com", 
+        firstname: "John", 
+        lastname: "Smith", 
+        phone: "1234567", 
+        address: "123 street"
+    }
+
     describe('getItems()', () => {
         it('gets item fom the items table', async () => {
             const itemArr = await items.getItems();
             expect(Array.isArray(itemArr)).toBe(true);
+        })
+    })
+    describe('addItem(item)', () => {
+        it('adds and returns the newly created item data', async () => {
+            const newUser = await users.add(mockUser);
+            const newItem = await items.addItem({ ...mockItem, id: newUser.id });
+            expect(newItem.title).toEqual(mockItem.title);
         })
     })
 })
