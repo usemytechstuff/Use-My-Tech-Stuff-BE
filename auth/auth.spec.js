db43fa547374c39b93258c4b026b7022d8d147a6
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-22 19:00:31 -0700

    auth POST second test pass

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -9,12 +9,31 @@ describe('auth tests', () => {
     afterEach(async () => {
         await db("users").truncate();
     });
+
+    const mockUser = {
+        username: "JohnSmith123", 
+        password: "password", 
+        email: "JohnSmith123@gmail.com", 
+        firstname: "John", 
+        lastname: "Smith", 
+        phone: "1234567", 
+        address: "123 street"
+    }
+
     describe('POST /register endpoint', () => {
         it('returns 201 if user is registered with required params', async () => {
             const response = await request(server)
                 .post('/api/register')
                 .send({ username: 'username', password: 'password' });
             expect(response.status).toEqual(201);
+        })
+        it('returns the newly created users with all entered params if succesful', async () => {
+            const response = await request(server)
+                .post('/api/register')
+                .send(mockUser);
+            expect(response.body.username).toEqual(mockUser.username);
+            expect(response.body.email).toEqual(mockUser.email);
+            expect(response.body.firstname).toEqual(mockUser.firstname);
         })
     })
 })
