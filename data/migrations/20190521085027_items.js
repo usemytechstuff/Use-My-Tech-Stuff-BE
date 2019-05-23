ac172eb629d62d6152d71581c6d68c805346fb5f
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-23 16:25:23 -0700

    rdp, changed longtext bc doesnt work with pg

--- data/migrations/20190521085027_items.js
+++ data/migrations/20190521085027_items.js
@@ -16,7 +16,7 @@ exports.up = function(knex, Promise) {
         .string("type")
         .notNullable();
       item
-        .longtext("description")
+        .text("description", 'longtext')
         .notNullable();
       item
         .integer("price")
@@ -29,7 +29,7 @@ exports.up = function(knex, Promise) {
       item
         .string("model");
       item
-        .longtext("imgURL");
+        .text("imgURL", 'longtext');
       item
         .integer("renter")
         .unsigned()
